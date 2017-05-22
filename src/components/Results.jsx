5b1f4d895ad7553028419c2a3885ad7622c4c8d7
Michael Pang <michaelpang@Michaels-MacBook-Pro.local>
2017-05-22 20:39:57 +0200

    update to using socket.io

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -5,56 +5,56 @@ import { connect } from 'react-redux';
 import Winner from './Winner';
 
 export class Results extends React.Component {
-    constructor(props) {
-        super(props)
-    }
+  constructor(props) {
+    super(props)
+  }
 
-    shouldComponentUpdate(nextProps, nextState) {
-        return shallowCompare(this, nextProps, nextState)
-    }
+  shouldComponentUpdate(nextProps, nextState) {
+    return shallowCompare(this, nextProps, nextState)
+  }
 
-    getPair() {
-        return this.props.pair || [];
-    }
+  getPair() {
+    return this.props.pair || [];
+  }
 
-    getVotes(entry) {
-        if(this.props.tally && this.props.tally.has(entry)) {
-            return this.props.tally.get(entry);
-        }
-        return 0;
+  getVotes(entry) {
+    if(this.props.tally && this.props.tally.has(entry)) {
+      return this.props.tally.get(entry);
     }
+    return 0;
+  }
 
-    render() {
-        return this.props.winner ?
-        <Winner ref="winner" winner={this.props.winner} /> :
-            <div className="results">
-                <div className="tally">
-                    {this.getPair().map(entry =>
-                        <div key={entry} className="entry">
-                            <h1>{entry}</h1>
-                            <div className="voteCount">
-                                {this.getVotes(entry)}
-                            </div>
-                        </div>
-                    )}
-                </div>
-                <div className="management">
-                    <button ref="next"
-                        className="next"
-                        onClick={this.props.next}
-                        >
-                        Next
-                    </button>
-                </div>
+  render() {
+    return this.props.winner ?
+    <Winner ref="winner" winner={this.props.winner} /> :
+      <div className="results">
+        <div className="tally">
+          {this.getPair().map(entry =>
+            <div key={entry} className="entry">
+              <h1>{entry}</h1>
+              <div className="voteCount">
+                {this.getVotes(entry)}
+              </div>
             </div>
+          )}
+        </div>
+        <div className="management">
+          <button ref="next"
+            className="next"
+            onClick={this.props.next}
+            >
+            Next
+          </button>
+        </div>
+      </div>
     }
-};
+  };
 
-function mapStateToProps(state) {
-return {
-pair: state.getIn(['vote', 'pair']),
-tally: state.getIn(['vote', 'tally']),
-winner: state.get('winner')
-}
-}
-export const ResultsContainer = connect(mapStateToProps)(Results);
+  function mapStateToProps(state) {
+    return {
+      pair: state.getIn(['vote', 'pair']),
+      tally: state.getIn(['vote', 'tally']),
+      winner: state.get('winner')
+    }
+  }
+  export const ResultsContainer = connect(mapStateToProps)(Results);
